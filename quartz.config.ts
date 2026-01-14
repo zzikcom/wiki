ee9955ae6c7e33e61fe47e0fc3ce8fd96b3395a6
zzik.com <zzik.com@messymail.com>
2026-01-14 13:02:41 +0900

    added an emoji in the title

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Zzikipedia",
+    pageTitle: "🌵Zzikipedia",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
