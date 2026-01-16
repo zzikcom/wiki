73aed0566e207e6fb79cc119bc137d9546583d0e
zzik.com <zzik.com@messymail.com>
2026-01-16 17:52:32 +0900

    modify CrawlLinks setting

--- quartz.config.ts
+++ quartz.config.ts
@@ -64,7 +64,10 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.CrawlLinks({
+        markdownLinkResolution: "shortest",
+        openLinksInNewTab: true,
+      }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
